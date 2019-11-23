f698c507e538aed77a5fbeb969873b5aed5c6ad9
shravanc <shravan007.c@gmail.com>
2019-11-23 20:04:32 +0000

    list service update

--- portal/src/app/list-service.service.ts
+++ portal/src/app/list-service.service.ts
@@ -9,12 +9,23 @@ export class ListServiceService {
 
   constructor(private http: HttpClient) { }
 
+
+  host = "http://amazon.stream.com";
+  auth = "?auth_token=abc";
+  base_url = this.host + ':3000/admin/lists';
+  url = "";
   getMenuLists() {
-    return this.http.get('http://localhost:3000/lists/tab')
+    this.url  = this.base_url + '/tab' + this.auth
+    console.log("=========", this.url)
+    return this.http.get(this.url)
+
+    //return this.http.get(this.host + ':3000/lists/tab' + this.auth)
   }
 
   getList(list_id) {
-    return this.http.get('http://localhost:3000/lists/'+list_id)
+    this.url  = this.base_url + '/' +list_id + this.auth
+    console.log("***********", this.url)
+    return this.http.get( this.url)
   }
 
 
